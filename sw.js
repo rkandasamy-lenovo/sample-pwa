302191d7ee7d9a85f3bdabb4b0bedcf71ee04937
Ramakrishnan Kandasamy <rmkanda@thoughtworks.com>
2020-09-29 13:35:25 +0530

    Add additional files to cache

--- sw.js
+++ sw.js
@@ -1,5 +1,17 @@
 var cacheName = "hello-pwa";
-var filesToCache = ["./", "./index.html", "./css/style.css", "./js/main.js"];
+var filesToCache = [
+  "./",
+  "./index.html",
+  "./css/style.css",
+  "./js/main.js",
+  "./manifest.json",
+  "./favicon.ico",
+  "./images/hello-icon-152.png",
+  "//cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js",
+  "//code.jquery.com/jquery-3.5.1.min.js",
+  "//stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css",
+  "//stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js",
+];
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener("install", function (e) {
